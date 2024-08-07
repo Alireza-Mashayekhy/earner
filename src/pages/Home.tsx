90ab4cb35f9432d215c34cb4076fa7b2b0cb0be8
alireza <alirezamashayekhy2000@gmail.com>
2024-08-07 21:25:51 +0330

    dashboard page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,26 @@
-import { ReactElement } from "react";
-import { Link } from "react-router-dom";
-
-import {
-  ArrowLeft2,
-  Discover,
-  Eye,
-  Messages,
-  VideoVertical,
-} from "iconsax-react";
-
 export default function Home() {
-  interface itemType {
-    icon: ReactElement;
-    title: string;
-    link: string;
-  }
-  const items: itemType[] = [
-    {
-      icon: <Messages color="#26e6b2" size={50} />,
-      title: "تیکت های جدید",
-      link: "/",
-    },
-    {
-      icon: <VideoVertical color="#26e6b2" size={50} />,
-      title: "ویدئو های جدید",
-      link: "/",
-    },
-    {
-      icon: <Eye color="#26e6b2" size={50} />,
-      title: "لینک های جدید",
-      link: "/",
-    },
-    {
-      icon: <Discover color="#26e6b2" size={50} />,
-      title: "ماموریت های مجاز جدید",
-      link: "/",
-    },
-  ];
   return (
-    <div className="flex flex-col gap-7">
-      <h1 className="text-2xl font-bold">پیشخوان</h1>
-      <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
-        {items.map((item) => {
-          return (
-            <Link
-              to={item.link}
-              key={item.title}
-              className="border-primary-2 flex flex-col items-center gap-3 rounded-[12px] border bg-primary-1 py-[20px]"
-            >
-              {item.icon}
-              <span className="text-center">{item.title}</span>
-              <div className="flex items-center text-sm text-gray-400">
-                مشاهده
-                <ArrowLeft2 size={10} className="relative right-1" />
-                <ArrowLeft2 size={10} />
-              </div>
-            </Link>
-          );
-        })}
+    <div className="grid grid-cols-3 gap-3">
+      <div className="col-span-2 flex items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
+      </div>
+      <div className="flex aspect-square items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
+      </div>
+      <div className="flex aspect-square items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
+      </div>
+      <div className="flex aspect-square items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
+      </div>
+      <div className="flex aspect-square items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
+      </div>
+      <div className="flex aspect-square items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
+      </div>
+      <div className="col-span-2 flex items-center justify-center rounded-xl bg-gray-300 text-black">
+        AD
       </div>
     </div>
   );
